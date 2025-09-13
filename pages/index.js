a35341dba7dea92df83316953a4d5115877a61a7
Leonardo Lima <84804856+Leonardo-Lima27@users.noreply.github.com>
2025-09-13 13:26:31 +0000

    scripts `lint:check` and `lint:fix` added.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Hoje é costa, biceps e triceps. Yeah Babeeeeee LightWeight! 💪</h1>
+  return (
+    <h1>Hoje é costa, biceps e triceps. Yeah Babeeeeee LightWeight! 💪</h1>
+  );
 }
 
 export default Home;
